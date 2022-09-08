01a8be8444ce4c0c364f5b4b851b7749e7090568
Nicolay J <nicolay.dev@outlook.com>
2022-09-08 16:27:24 -0500

    adding simulation for voting process

--- app.py
+++ app.py
@@ -1,20 +1,64 @@
+from urllib import response
 import urllib3
+import json
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eporra.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['JWT_SECRET_KEY'] = 'frase-secreta'
-# app.config['PROPAGATE_EXCEPTIONS'] = True
 
 app_context = app.app_context()
 app_context.push()
 
 cors = CORS(app)
 
+urlServiceTest = 'https://jsonplaceholder.typicode.com/todos/1'
+# urlServiceOne = urlServiceTest
+# urlServiceTwo = urlServiceTest
+# urlServiceThree = urlServiceTest
+urlServiceOne = 'http://127.0.0.1:5000/'
+urlServiceTwo = 'http://127.0.0.1:5000/'
+urlServiceThree = 'http://127.0.0.1:5000/'
+
 @app.route('/sendAlarm')
-def sendAlarm():
+def sendAlarmRequest():
+    response1 = sendAlarm(urlServiceOne)
+    response2 = sendAlarm(urlServiceOne)
+    response3 = sendAlarm(urlServiceOne)
+    status = compareResult(response1, response2, response3)
+
+    if (status != 'ok'):
+        if ((response3 != response1) & (response3 != response2) & (response1 != response2)):
+            response = buildResponse(status, '', '{} {} {}'.format(urlServiceOne, urlServiceTwo, urlServiceThree))
+        if (response3 == response1 != response2):
+            response = buildResponse(status, response3, urlServiceTwo)
+        if (response3 != response1 == response2):
+            response = buildResponse(status, response1, urlServiceThree)
+        if (response1 != response3 == response2):
+            response = buildResponse(status, response2, urlServiceOne)
+    else:
+        response = buildResponse(status, response1, '')
+    return json.dumps(response)
+
+def sendAlarm(url):
     http = urllib3.PoolManager()
-    req = http.request('GET', 'https://jsonplaceholder.typicode.com/todos/1')
-    return req.data
+    req = http.request('GET', url)
+    result = json.loads(req.data)
+    # validate the response from alarmManager microservice
+    return result
+
+def compareResult(data1, data2, data3):
+    if (data1 == data2 == data3):
+        status = 'ok'
+    else:
+        status = 'fail'
+    return status
+
+def buildResponse(status, data, serviceFail):
+    response = {
+        "status": status,
+        "data": data,
+        "serviceFail": 'none'
+    }
+    if (status != 'ok'):
+        response['serviceFail'] = serviceFail
+    return response
