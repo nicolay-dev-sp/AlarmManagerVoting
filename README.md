7cc58cbe8db3491379cc95472c604467516e2bcf
Nicolay J <nicolay.dev@outlook.com>
2022-09-09 18:22:16 -0500

    updating readme & configuring multple services

--- README.md
+++ README.md
@@ -4,4 +4,13 @@
 
 Este micorservicio se encarga de recibir una solicitud de consulta que a su vez es procesada por tres microservicios de "AlarmManager" para posteriormente procesar los resultados.
 
-### Táctica de detección de errores por votación
+### Ejecución del experimento
+
+1. Se deben crear tres instacias diferentes del servicio de alarmas (https://github.com/leslysharyn/MISW4202-Grupo-27) con puertos de escucha diferentes, para el ejemplo se utilizaron los puertos: 8100, 8200, 8300
+2. Configurar las variables del archivo app.py (urlServiceOne, urlServiceTwo, urlServiceThree) para que coincidan con las url de los tres microservicios ya desplegados
+3. Cargar el archivo de ejecución "HTTP Procesar Alarma.jmx" en jmeter
+4. Ejecutar la operación en jmeter con el botón verde
+
+**Nota:** Para evidenciar los cambios en la disponibilidad del experimento usando las tacticas planteadas, se deben configurar las variables voteTactic y retryHU en "True" en el archivo app.py, reiniciar el servidor y lanzar de nuevo la ejecución en jmeter.
+
+### Táctica de detección de errores por votación y camuflaje por reintentos
--- app.py
+++ app.py
@@ -15,18 +15,24 @@ urlServiceTest = 'https://jsonplaceholder.typicode.com/todos/1'
 # urlServiceOne = urlServiceTest
 # urlServiceTwo = urlServiceTest
 # urlServiceThree = urlServiceTest
-urlServiceOne = 'http://127.0.0.1:5000/'
-urlServiceTwo = 'http://127.0.0.1:5000/'
-urlServiceThree = 'http://127.0.0.1:5000/'
+urlServiceOne = 'http://127.0.0.1:8100/alarmaprocesada'
+urlServiceTwo = 'http://127.0.0.1:8200/alarmaprocesada'
+urlServiceThree = 'http://127.0.0.1:8300/alarmaprocesada'
+retryHU = True
+voteTactic = True
+amountOfRetries = 7
 
 @app.route('/sendAlarm')
 def sendAlarmRequest():
     response1 = sendAlarm(urlServiceOne)
-    response2 = sendAlarm(urlServiceOne)
-    response3 = sendAlarm(urlServiceOne)
+    response2 = sendAlarm(urlServiceTwo)
+    response3 = sendAlarm(urlServiceThree)
     status = compareResult(response1, response2, response3)
 
+    resStatus = 200
+
     if (status != 'ok'):
+        resStatus = 404
         if ((response3 != response1) & (response3 != response2) & (response1 != response2)):
             response = buildResponse(status, '', '{} {} {}'.format(urlServiceOne, urlServiceTwo, urlServiceThree))
         if (response3 == response1 != response2):
@@ -37,7 +43,7 @@ def sendAlarmRequest():
             response = buildResponse(status, response2, urlServiceOne)
     else:
         response = buildResponse(status, response1, '')
-    return json.dumps(response)
+    return json.dumps(response), resStatus
 
 def sendAlarm(url):
     http = urllib3.PoolManager()
